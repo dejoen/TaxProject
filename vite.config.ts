00cfef6cb78fce7a30d7fbaa97be9b98ca4b3756
Devjoe <benagu477@gmail.com>
2024-09-23 21:25:27 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [react()],
-  base:'https://taxproject.onrender.com/dashboard'
+  base:'dashboard',
+ 
+})
+*/
+
+export default defineConfig(({ command,}) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+    
+      // dev specific config
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
+      
+      // build specific config
+    }
+  }
 })
